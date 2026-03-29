c2da2a9b9021a8a05bc02a74eb077e69e7e8fbff
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-03-29 18:33:35 +0300

    feat : init project

---
+++ src/main/java/hei/td5ingredient/Td5IngredientApplication.java
@@ -0,0 +1,13 @@
+package hei.td5ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td5IngredientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td5IngredientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/td5ingredient/Td5IngredientApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.td5ingredient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5IngredientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
